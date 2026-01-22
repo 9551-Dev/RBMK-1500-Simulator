91761774c010a958be9e7e029db62e27bff30623
9551-Dev <9551dev@gmail.com>
2026-01-22 16:59:34 +0100

    fix build script

--- linux_run_build.sh
+++ linux_run_build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-latest_jar=$(ls -t RBMK-1500-Simulator-*.jar | head -1)
+latest_jar=$(ls -t ./target/RBMK-1500-Simulator-*.jar | head -1)
 
 if [ -f "$latest_jar" ]; then
     echo "Running: $latest_jar"
