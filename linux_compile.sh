3fd88e5120f8935a536ea7c2a32afb25fe9f5918
9551-Dev <9551dev@gmail.com>
2026-01-22 17:52:32 +0100

    fix linker

--- linux_compile.sh
+++ linux_compile.sh
@@ -3,7 +3,7 @@ set -e
 
 mvn clean package
 
-JAR_FILE=$(find target -name "*jar-with-dependencies.jar" | head -1)
+JAR_FILE=$(find target -name "RBMK-1500-Simulator-*.jar" | head -1)
 
 if [ -n "$JAR_FILE" ] && [ -f "$JAR_FILE" ]; then
     ln -sf "$(pwd)/$JAR_FILE" "./latest-simulator.jar"
